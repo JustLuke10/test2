48b4d8eda9c8da12bef92e9714a20b5fcd006a1d
JustLuke10 <163893905+JustLuke10@users.noreply.github.com>
2024-03-18 17:59:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching Splatoon","Monitoring the D4D server","D4D Official Bot"];
 
 
 let currentIndex = 0;
